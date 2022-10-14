829e75947dbaa54f887887a81fbde5e375b88f90
Felipe Belisario <felipesantos.dsb@hotmail.com>
2022-10-13 21:00:13 -0300

    add li element and button

--- lesson5/scripts/script.js
+++ lesson5/scripts/script.js
@@ -7,12 +7,46 @@ const list = document.querySelector('list');
 // Create an click event listener for the Add Chapter button using addEventListener and an anonymous function.  
 // Example:  button.addEventListener('click', function() { ...
 // In the function block for adding a chapter, make sure the input is not blank and if not, do the following:
-// create an li element
-// create a delete button
+button.addEventListener('click', () => {
+    const item = input.value;
+    input.value= '';
+
+    // create an li element
+    const listItem = document.createElement('li');
+    const listText = document.createElement('span');
+    // create a delete button
+    const listBtn = document.createElement('button');
+
+    listItem.appendChild(listText);
+    
+
+
+})
+
+
+
+
+
+
+
+
+
 // populate the li elements textContent or innerHTML with the input
+
+
 // populate the button textContent with an ❌
+
+
 // append the li element with the delete button
+
+
 // append the list element with the li element just created and appended with text and the delete button
+
+
 // add an event listener to the delete button that removes the li element when clicked
+
+
 // send the focus to the input element
+
+
 // clean up the successful add of a chapter by changing the input to nothing or the empty string and setting the focus to the input.
