8030b51da62a1180b62503d44e853637f2af3b30
Felipe Belisario <felipesantos.dsb@hotmail.com>
2022-10-15 00:12:33 -0300

    add js feature

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -21,3 +21,20 @@ function toggleMenu() {
   
   const x = document.getElementById('hamburgerBtn');
   x.onclick = toggleMenu;
+
+  // Use JavaScript to display a banner on Mondays or Tuesdays 
+  //only at the very top of the page that says "🤝🏼 
+  //Come join us for the chamber meet and greet Wednesday at 7:00 p.m."
+  // Make sure your design matches your schema for the site.
+
+const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const currentWeekDay = week[date.getDay()];
+if (currentWeekDay === 'Tuesday' || currentWeekDay === 'Wednesday'){ 
+  // currentWeekDay === 'Saturday' => used for testing
+  document.getElementById('hidden').id = 'show';
+  document.getElementById('show').id = 'hidden';
+  
+    
+    };
+  
+  
