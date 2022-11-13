81e400e2e6d3b3e6f0d6f2cbb333781722b0de3f
Felipe <felipesantos.dsb@hotmail.com>
2022-11-12 22:02:54 -0300

    update files

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,37 +1,41 @@
+const requestURL = '/chamber/scripts/data.json'
 const cards = document.querySelector('.cards');
 
-fetch('/chamber/scripts/data.json')
-    .then(function(response){
+fetch(requestURL)
+    .then(function (response) {
         return response.json();
     })
-    .then(function(jsonObject){
+    .then(function (jsonObject) {
         const companies = jsonObject['companies'];
         companies.forEach(displayCompanies);
     })
 
-function displayCompanies(company){
+function displayCompanies(company) {
     let card = document.createElement('section');
     let companyName = document.createElement('h2');
     let image = document.createElement('img');
     let address = document.createElement('h3');
     let phone = document.createElement('h3');
-    let website = document.createElement('h3')
+    let website = document.createElement('a')
 
-    companyName.textContent = companies.name;
-    address.textContent = `Address: ${companies.address}`;
-    phone.textContent = companies.phone;
-    website.textContent = companies.website;
- 
+    companyName.textContent = company.name;
+    address.textContent = company.address;
+    phone.textContent = company.phone;
+    
+    website.setAttribute('href', `https://${company.website}`);
+    website.setAttribute('target', '_blank')
+    website.textContent = company.website;
+
     image.setAttribute('src', company.image);
     image.setAttribute('alt', `Image of ${company.name}`);
     image.setAttribute('loading', 'lazy');
 
-    card.appendChild(image);
+    // card.appendChild(image);
     card.appendChild(companyName);
     card.appendChild(address);
     card.appendChild(phone);
     card.appendChild(website);
-    
+
     document.querySelector('div.cards').appendChild(card)
 
 }
